c46bddf9a89d019ccf83441474c5865e08ee9439
Kinggygvh <newresultbadshah@gmail.com>
2025-05-14 10:52:43 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -111,5 +111,9 @@ def predict():
     is_phishing = prediction[0] == 1
     return jsonify({'is_phishing': is_phishing})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's dynamic port or fallback to 5000
+    app.run(host='0.0.0.0', port=port)
+
